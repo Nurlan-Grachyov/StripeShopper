3c9f61acc1942513efdc6737d3a2f899740c9cb7
Nurlan-Grachyov <nurlan.grachyov@mail.ru>
2025-07-29 23:37:42 +0400

    added url "/"

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse("Главная страница сайта")),
     path("products/", include("products.urls", namespace="products")),
     path("orders/", include("orders.urls", namespace="orders")),
-
 ]
